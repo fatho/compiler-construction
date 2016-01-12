a7a9ef2df22f1f7c94d978f7298df73ec6af7e1c
Jordy van Dortmont <ydroj@live.nl>
2016-01-12 11:16:27 +0100

    implemented program to labeled program conversion and flow synthesis

--- mf/src/Main.hs
+++ mf/src/Main.hs
@@ -11,4 +11,8 @@ import Control.Arrow    (arr, (>>>))
 parser :: Component String AG.Program
 parser = arr $ Parser.happy . Lexer.alex
 
-main = ioWrap (parser >>> printer)
+main = ioWrap (parser >>> arr toLabeledProgram >>> printer)
+
+toLabeledProgram :: AG.Program -> AG.Program'
+toLabeledProgram prog = 
+    AG.prog_Syn_Program $ AG.wrap_Program (AG.sem_Program prog) AG.Inh_Program
